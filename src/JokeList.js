256a23d0687cc423d8fe1a46e48f057d8b0113e6
pankaj-pant <pankajpant89@gmail.com>
2020-06-25 21:56:18 +0300

    fetching 10 jokes and displaying

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios'
 
 const JokeList = () => {
-    const fetchJoke = () => {
-        axios.get("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}})
-            .then(response => console.log(response.data.joke))
-            .catch(error => console.log(error))
+    const [jokes, setJokes] = useState([])
+
+    const fetchJoke = async () => {
+        let i = 0
+        let fetchedJokes =[]
+        while (i < 10) {
+            let response = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}})
+            console.log(response.data.joke)
+            fetchedJokes.push(response.data.joke)
+            i++
+        }
+        setJokes(fetchedJokes)
+        console.log("Fetched jokes:", i)
+
     }
     return (
         <div>
             <h1>Dad Jokes</h1>
             <button onClick={() => fetchJoke()}>Fetch Joke</button>
+            <ul>
+                {jokes.map(j => <li key={j}>{j}</li>)}
+            </ul>
         </div>
     )
 }
