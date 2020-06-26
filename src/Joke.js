cb1c6cea2121ad751422dc5dde6bba9904a7db93
pankaj-pant <pankajpant89@gmail.com>
2020-06-26 14:52:54 +0300

    individual Joke styling

--- src/Joke.js
+++ src/Joke.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import './Joke.css'
 
 const Joke = ({joke, votes, id, updateVotes}) => {
     return (
         <div className="Joke">
             <div className="Joke-buttons">
-                <button onClick={ () => updateVotes(id, votes + 1)}>
-                    <i className="fas fa-arrow-up" />
-                </button>
-                <span>{votes}</span>
-                <button onClick={ () => updateVotes(id, votes - 1)}>
-                    <i className="fas fa-arrow-down" />
-                </button>
+                <i className="fas fa-arrow-up" onClick={ () => updateVotes(id, votes + 1)}/>
+                <span className="Joke-votes">{votes}</span>
+                <i className="fas fa-arrow-down" onClick={ () => updateVotes(id, votes - 1)}/>
             </div>
             <div className="Joke-text">{joke}</div>
+            <div className="Joke-smiley">
+            <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+            </div>
         </div>
     )
 }
